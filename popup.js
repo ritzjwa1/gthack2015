bb82840a6b0e2bcc7c280e1eafc8e238b099ad7e
ritzjwa <ritzeetech1T>
2015-09-26 14:52:15 -0400

    h

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+// Install necessary modules
+// npm install unirest
+
 /**
  * Get the current URL.
  *
@@ -44,4 +47,31 @@ function getCurrentTabUrl(callback) {
     if (request.openUrlInEditor)
       openUrl(request.openUrlInEditor);
   }); 
+}
+
+function getTitle (url) {
+  // These code snippets use an open-source library.
+  var unirest = require('unirest');
+var title = unirest.post("https://neutrinoapi-html-extract.p.mashape.com/html-extract-tags")
+.header("X-Mashape-Key", "FAwe0YDMHSmsh3yFveT2sNYT86YYp1FqvMEjsn0yT9XGXekjlD")
+.header("Content-Type", "application/x-www-form-urlencoded")
+.header("Accept", "application/json")
+.send("content=http://www.neutrinoapi.com/")
+.send("tag=title")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
+}
+
+function getHyperlinks(url) {
+  var unirest = require('unirest');
+  var hyperlinks = unirest.post("https://neutrinoapi-html-extract.p.mashape.com/html-extract-tags")
+  .header("X-Mashape-Key", "FAwe0YDMHSmsh3yFveT2sNYT86YYp1FqvMEjsn0yT9XGXekjlD")
+  .header("Content-Type", "application/x-www-form-urlencoded")
+  .header("Accept", "application/json")
+  .send("content=http://www.neutrinoapi.com/")
+  .send("tag=a")
+  .end(function (result) {
+    console.log(result.status, result.headers, result.body);
+  });
 }
